88c921370ebd8c307344abef31dd528c0445780d
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-24 10:10:13 +0700

    first commit

---
+++ src/main/java/com/qiraht/ticket_order/TicketOrderApplication.java
@@ -0,0 +1,13 @@
+package com.qiraht.ticket_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qiraht/ticket_order/TicketOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiraht.ticket_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
