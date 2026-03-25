3bb3b3f8e901ebafa955e8d12eae582dbb273c6b
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-26 02:15:08 +0700

    feat: add getEvents and getEventById in EventController

--- src/main/java/com/qiraht/ticket_order/controller/EventController.java
+++ src/main/java/com/qiraht/ticket_order/controller/EventController.java
@@ -1,17 +1,17 @@
 package com.qiraht.ticket_order.controller;
 
 import com.qiraht.ticket_order.dto.ApiResponse;
-import com.qiraht.ticket_order.dto.request.EventReqDTO;
+import com.qiraht.ticket_order.dto.request.EventRequest;
+import com.qiraht.ticket_order.dto.response.EventResponse;
 import com.qiraht.ticket_order.service.EventService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/events")
@@ -26,8 +26,8 @@ public class EventController {
     @PostMapping
     @Tag(name = "Events")
     @Operation(summary = "Create Event", description = "Create new Event. Authentication Needed and role 'ADMIN'")
-    public ResponseEntity<ApiResponse<String>> postEvent(@RequestBody EventReqDTO eventReqDTO) {
-        String data = eventService.createEvent(eventReqDTO);
+    public ResponseEntity<ApiResponse<String>> postEvent(@RequestBody EventRequest eventRequest) {
+        String data = eventService.createEvent(eventRequest);
 
         ApiResponse<String> body = ApiResponse.success(
                 HttpStatus.CREATED.value(),
@@ -36,5 +36,19 @@ public class EventController {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(body);
-    };
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<EventResponse>>> getEvents() {
+        List<EventResponse> data = eventService.getAllEvents();
+
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(), "success", data));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<EventResponse>> getEventById(@PathVariable("id") String eventId) {
+        EventResponse data = eventService.getEventById(eventId);
+
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(), "success", data));
+    }
 }
