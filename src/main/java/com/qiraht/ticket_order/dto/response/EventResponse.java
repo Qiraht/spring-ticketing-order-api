2a20fef5ccc37b71041d74f35b7fd01e12277e54
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-26 02:09:30 +0700

    feat: add EventResponse DTO

---
+++ src/main/java/com/qiraht/ticket_order/dto/response/EventResponse.java
@@ -0,0 +1,15 @@
+package com.qiraht.ticket_order.dto.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        String id,
+        String name,
+        String description,
+        LocalDateTime eventDate,
+        BigDecimal price,
+        Integer capacity,
+        Integer available
+) {
+}
