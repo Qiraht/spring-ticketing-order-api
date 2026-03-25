1fd5c207a099d7bb9f9aa10de38d4521a58a3b77
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-26 01:33:53 +0700

    feat: create EventRepository

---
+++ src/main/java/com/qiraht/ticket_order/repository/EventRepository.java
@@ -0,0 +1,11 @@
+package com.qiraht.ticket_order.repository;
+
+import com.qiraht.ticket_order.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
