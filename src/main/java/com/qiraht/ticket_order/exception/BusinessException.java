90043b3f571477a03cc4c50b65a17db4a0e8e571
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-26 01:30:13 +0700

    feat: create custom exception for BusinessException and NotFoundException

---
+++ src/main/java/com/qiraht/ticket_order/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.qiraht.ticket_order.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
