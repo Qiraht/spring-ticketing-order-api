ac75b52b4847e16943a1ad8e31f10f17f3402b9a
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-26 23:10:31 +0700

    migration: create events table migration

---
+++ src/main/resources/db/changelog/scripts/create_events_table.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+--changeset author:Thariq Aulia Akbar
+--comment: first event table version
+
+CREATE  TABLE IF NOT EXISTS events (
+    id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    location VARCHAR(100) NOT NULL,
+    event_date DATETIME NOT NULL,
+    price NUMERIC(5,2) NOT NULL,
+    capacity INT NOT NULL,
+    available INT NOT NULL,
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) NOT NULL,
+    deleted_at TIMESTAMP(3) NULL
+)
