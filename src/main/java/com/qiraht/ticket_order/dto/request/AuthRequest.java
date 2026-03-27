45b8c97f94300db724ae563c19ab68d8108dfe5a
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 23:10:48 +0700

    feat: create AuthRequest dto

---
+++ src/main/java/com/qiraht/ticket_order/dto/request/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.qiraht.ticket_order.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthRequest(
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
