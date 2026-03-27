62e445e04d299d1d5366884bee327cbdefac17a4
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-28 03:25:07 +0700

    feat: add calculateRevenueByEventId on TicketRepository

--- src/main/java/com/qiraht/ticket_order/repository/TicketRepository.java
+++ src/main/java/com/qiraht/ticket_order/repository/TicketRepository.java
@@ -2,12 +2,19 @@ package com.qiraht.ticket_order.repository;
 
 import com.qiraht.ticket_order.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
     List<Ticket> findAllByUserId(UUID userId);
+
+    @Query("SELECT SUM(t.priceAtSales * t.quantity) FROM Ticket t WHERE t.event.id = :eventId AND t.status = 'BOOKED'")
+    BigDecimal calculateRevenueByEventId(@Param("eventId") UUID eventId);
 }
+
