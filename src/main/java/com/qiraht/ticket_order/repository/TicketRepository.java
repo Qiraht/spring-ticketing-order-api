2bf2b8a5e049a7948a3a1db9a9c6849a6f9974cc
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 10:53:48 +0700

    feat: create TicketRepository

---
+++ src/main/java/com/qiraht/ticket_order/repository/TicketRepository.java
@@ -0,0 +1,11 @@
+package com.qiraht.ticket_order.repository;
+
+import com.qiraht.ticket_order.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+}
