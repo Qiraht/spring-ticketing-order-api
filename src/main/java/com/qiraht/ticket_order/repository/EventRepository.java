f7398864fd2ecd99e4852f30672bc81d3e2e7fe6
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-28 02:46:39 +0700

    feat: add findAllByDeletedAtNull on EventRepository

--- src/main/java/com/qiraht/ticket_order/repository/EventRepository.java
+++ src/main/java/com/qiraht/ticket_order/repository/EventRepository.java
@@ -4,8 +4,10 @@ import com.qiraht.ticket_order.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
+    List<Event> findAllByDeletedAtNull();
 }
