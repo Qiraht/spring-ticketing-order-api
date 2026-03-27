39c907101d233cd8a1f12cfa6fb7f2c9ef849cb7
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 23:09:36 +0700

    feat: create CustomUserDetails and CustomUserDetailsService for security

---
+++ src/main/java/com/qiraht/ticket_order/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.qiraht.ticket_order.service;
+
+import com.qiraht.ticket_order.config.CustomUserDetails;
+import com.qiraht.ticket_order.entity.User;
+import com.qiraht.ticket_order.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
