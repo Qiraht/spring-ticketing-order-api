d20f8eb332d66b329def97397e45daf31df3faf0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 10:55:24 +0700

    feat: create TicketRequest dto

---
+++ src/main/java/com/qiraht/ticket_order/dto/request/TicketRequest.java
@@ -0,0 +1,15 @@
+package com.qiraht.ticket_order.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record TicketRequest(
+        @NotBlank
+        String eventId,
+
+        @NotNull
+        @Positive
+        Integer quantity
+) {
+}
