e4baa49031f965307c38da6217c8e51a4c78ae71
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 15:24:59 +0700

    feat: rename columns for events table and fix formatting

--- src/main/resources/db/changelog/scripts/create_events_table.sql
+++ src/main/resources/db/changelog/scripts/create_events_table.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
---changeset id:1 author:Qiraht
+--changeset Qiraht:1
 --comment: first event table version
 
-CREATE  TABLE IF NOT EXISTS events (
+CREATE TABLE IF NOT EXISTS events (
     id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
     name VARCHAR(50) NOT NULL,
     description TEXT,
@@ -10,8 +10,8 @@ CREATE  TABLE IF NOT EXISTS events (
     event_date DATETIME NOT NULL,
     price NUMERIC(5,2) NOT NULL,
     capacity INT NOT NULL,
-    available INT NOT NULL,
+    available_slot INT NOT NULL,
     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     updated_at TIMESTAMP(3) NOT NULL,
     deleted_at TIMESTAMP(3) NULL
-)
+);
