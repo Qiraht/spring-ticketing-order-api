03176dea25b50b91a8ad35f17d882a5456d8f0fe
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 23:10:29 +0700

    feat: create PasswordEncoder and AuthenticationManager config and setup Spring Security access

---
+++ src/main/java/com/qiraht/ticket_order/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.qiraht.ticket_order.config;
+
+import com.qiraht.ticket_order.handler.CustomAccessDeniedHandler;
+import com.qiraht.ticket_order.handler.UnAuthenticationHandler;
+import com.qiraht.ticket_order.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final UnAuthenticationHandler unAuthenticationHandler;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter,
+                          CustomUserDetailsService userDetailsService,
+                          CustomAccessDeniedHandler customAccessDeniedHandler,
+                          UnAuthenticationHandler unAuthenticationHandler
+    ) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userDetailsService = userDetailsService;
+        this.customAccessDeniedHandler= customAccessDeniedHandler;
+        this.unAuthenticationHandler = unAuthenticationHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(userDetailsService).authorizeHttpRequests(auth ->
+                        auth.requestMatchers(
+                                "/login",
+                                "/register",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated())
+                .exceptionHandling(ex ->
+                        ex.accessDeniedHandler(customAccessDeniedHandler)
+                                .authenticationEntryPoint(unAuthenticationHandler))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
