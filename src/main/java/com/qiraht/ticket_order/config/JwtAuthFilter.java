aed3b456e01a4171ab1cad5f67cd0a8e84d55fec
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-28 00:55:44 +0700

    feat: add logging for JwtAuthFilter

--- src/main/java/com/qiraht/ticket_order/config/JwtAuthFilter.java
+++ src/main/java/com/qiraht/ticket_order/config/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
@@ -39,7 +41,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             try {
                 email = jwtUtil.extractUsername(jwt);
             } catch (Exception e) {
-
+                log.debug("Invalid JWT token: {}", e.getMessage());
             }
         }
 
