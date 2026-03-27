24deef32091a4b6312ab556a9b8e3a98f9867c3a
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 23:06:03 +0700

    feat: create postRegister on UserController

--- src/main/java/com/qiraht/ticket_order/controller/UserController.java
+++ src/main/java/com/qiraht/ticket_order/controller/UserController.java
@@ -2,7 +2,10 @@ package com.qiraht.ticket_order.controller;
 
 import com.qiraht.ticket_order.dto.ApiResponse;
 import com.qiraht.ticket_order.dto.request.UserRequest;
+import com.qiraht.ticket_order.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/register")
+@Validated
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @PostMapping
-    public ResponseEntity<ApiResponse<String>> postRegister(@RequestBody UserRequest request) {}
+    public ResponseEntity<ApiResponse<String>> postRegister(@RequestBody UserRequest request) {
+        String data =  userService.registerUser(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.success(
+                        HttpStatus.CREATED.value(),
+                        "User registered successfully",
+                        data
+                )
+        );
+    }
 }
