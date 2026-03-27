ce85b66507e5791ce2f7fc15c00caffaa7d8e319
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 12:23:17 +0700

    feat: create TicketResponse DTO

---
+++ src/main/java/com/qiraht/ticket_order/dto/response/TicketResponse.java
@@ -0,0 +1,14 @@
+package com.qiraht.ticket_order.dto.response;
+
+import com.qiraht.ticket_order.constant.TicketStatus;
+
+import java.math.BigDecimal;
+
+public record TicketResponse(
+        String id,
+        EventResponse event,
+        BigDecimal priceAtSales,
+        Integer quantity,
+        TicketStatus status
+) {
+}
