b9e7c6712459addc2bb9bd1a2127bafd8cd3f4b5
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-27 15:26:55 +0700

    migration: create tickets table migrations

---
+++ src/main/resources/db/changelog/scripts/create_tickets_table.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+--changeset Qiraht:1
+--comment: first event table version
+
+CREATE TABLE IF NOT EXISTS tickets (
+    id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+    event_id VARCHAR(36) NOT NULL,
+    price NUMERIC(5,2) NOT NULL,
+    quantity INT NOT NULL,
+    status ENUM('BOOKED', 'CANCELLED') NOT NULL,
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+);
